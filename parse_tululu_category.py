beb46287e536238cfa9d85f298b745cfc368b6f5
e13q <110967581+e13q@users.noreply.github.com>
2024-09-05 21:29:53 +0300

    naming fix

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -18,14 +18,14 @@ def parse_page_for_pages_count(books_on_page):
 
 
 def get_books_links_by_category(category, page_start, page_end=None):
-    url_book_category = urljoin(BASE_URL, category)
+    book_category_url = urljoin(BASE_URL, category)
     if not page_end:
-        page_with_books = fetch_data(url_book_category)
+        page_with_books = fetch_data(book_category_url)
         page_end = parse_page_for_pages_count(page_with_books.text)
     links = []
     for i in range(page_start, page_end+1):
-        url_book_category_page = urljoin(url_book_category, f'{i}/')
-        page_with_books = fetch_data(url_book_category_page)
+        book_category_page_url = urljoin(book_category_url, f'{i}/')
+        page_with_books = fetch_data(book_category_page_url)
         links_on_page = parse_page_for_urls(page_with_books.text)
         links += links_on_page
     return links
